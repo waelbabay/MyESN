0b1a4dac3bb3815afdb853b55f734cb66b5ca738
Wael Babay <waelbabay@outlook.com>
2020-12-01 22:47:53 +0100

    init project

---
+++ MyESN.Core.Tests/Employee/EmployeeTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace MyESN.Core.Employee
+{
+    public class EmployeeTests
+    {
+        [Fact]
+        public void ShouldReturnEmployeeResultWithRequestValues()
+        {
+
+        }
+
+
+    }
+}
